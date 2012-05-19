91f984e26861cfbe1185213e0ebe76bb7bc3079a
romix <romixlev@gmail.com>
2012-05-19 15:36:15 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-akka-quickser-serialization
-===========================
+akka-quickser-serialization - quickser-based serializers for Akka
+=====================================================================
 
-quickser based serializers for Akka
+This library provides a custom quickser-based serializer for Akka. It can be used for more efficient akka actor's remoting. 
+
+Features
+--------
+
+*   It is more efficient than Java serialization - both in size and speed
+*   It can be faster than Kryo and protostuff based serializers in many cases 
+*   Does not require any additional build steps like compiling proto files, when using protobuf serialization
+*   Almost any Scala and Java class can be serialized using it without any additional configuration or code changes
+*   Apache 2.0 license
+
+How to use this library in your project
+----------------------------------------
+
+To use this serializer, you need to do two things:
+*   Include a dependency on this library into your project:
+
+	`libraryDependencies += "com.romix.akka" % "akka-quickser-serialization" % "0.1-SNAPSHOT"`
+    
+*   Add some new elements to your Akka configuration file, e.g. `application.conf`
+
+
+Configuration of akka-quickser-serialization in Akka configuration file
+-----------------------------------------------------------------------
+
+The following options are available for configuring this serializer:
+
+*   You should declare in the Akka `serializers` section a new kind of serializer:  
+
+		serializers {  
+			java = "akka.serialization.JavaSerializer"  
+			# Define protostuff serializer   
+			quickser = "com.romix.akka.serialization.quickser.QuickserSerializer"  
+		}    
+     
+*    As usual, you should declare in the Akka `serialization-bindings` section which classes should use quickser serialization
---
+++ src/main/scala/com/romix/akka/serialization/quickser/QuickserSerializer.scala
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright 2012 Roman Levenstein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.romix.akka.serialization.quickser
+
+import akka.serialization._
+import akka.actor.ExtendedActorSystem
+import akka.event.Logging
+
+import com.romix.quickser.Serialization;
+import com.typesafe.config.ConfigFactory
+import java.util.Map
+import scala.collection.JavaConversions._
+
+import java.lang.System
+
+import QuickserSerialization._
+
+import com.romix.quickser.Serialization
+
+class QuickserSerializer (val system: ExtendedActorSystem) extends Serializer {
+
+import QuickserSerialization._
+	val log = Logging(system, getClass.getName) 
+
+	val settings = new Settings(system.settings.config)
+
+	val mappings = settings.ClassNameMappings
+
+	locally {
+		log.debug("Got mappings: {}", mappings)
+	}
+
+	val classnames = settings.ClassNames
+
+	locally {
+		log.debug("Got classnames for incremental strategy: {}", classnames)
+	}
+
+	val bufferSize = settings.BufferSize
+
+	locally {
+		log.debug("Got buffer-size: {}", bufferSize)
+	}
+
+	val serializer = new Serialization();
+
+	locally {
+		log.debug("Got serializer: {}", serializer)
+	}
+
+
+	// This is whether "fromBinary" requires a "clazz" or not
+	def includeManifest: Boolean = false
+
+	// A unique identifier for this Serializer
+	def identifier = 123454322
+
+		
+	// "toBinary" serializes the given object to an Array of Bytes
+	def toBinary(obj: AnyRef): Array[Byte] = {
+		serializer.serialize(obj)
+	}
+
+	// "fromBinary" deserializes the given array,
+	// using the type hint (if any, see "includeManifest" above)
+	// into the optionally provided classLoader.
+	def fromBinary(bytes: Array[Byte], clazz: Option[Class[_]]): AnyRef = {
+		serializer.deserialize(bytes)
+	}
+}
